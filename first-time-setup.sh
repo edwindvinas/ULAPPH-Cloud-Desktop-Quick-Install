3d2a74404e255ea525227eeacf23fd5df64c529a
edwin.d.vinas <edwin.d.vinas@gmail.com>
2022-03-18 18:24:47 +0000

    updated

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -46,19 +46,19 @@ echo -e "${YELLOW}$THIS"
 echo -e "${NC}$THIS_ABOUT"
 echo -e "${NC}$THIS_GIT"
 echo -e "${RED}Downloading...${NC}${CYAN} $THIS  ${NC}\n"
-PWD=`pwd`
-echo "Your Current Directory: " $PWD
-MAIN=`cd .. && pwd`
-echo "Will clone under Main Directory: " $MAIN
-OLD=${MAIN}/${THIS}
+#PWD=`pwd`
+#echo "Your Current Directory: " $PWD
+#MAIN=`cd .. && pwd`
+#echo "Will clone under Main Directory: " $MAIN
+#OLD=${MAIN}/${THIS}
 echo "Removing old directory: " $OLD
-rm -rf ${OLD}
-cd $MAIN && git clone $THIS_GIT
+rm -rf ${THIS}
+git clone $THIS_GIT
 echo -e "${GREEN}Downloaded... ${NC}${CYAN} $THIS  ${NC}\n"
 echo "========================================"
 
 echo "========================================"
 echo -e "${CYAN}Listing directories... ${NC}\n"
-cd $MAIN && ls -la 
-echo -e "You can go to the below directory: \n${GREEN}$MAIN${NC}\n"
+ls -la 
+echo -e "You can go to the below directory: \n${GREEN}$MAIN/edwindvinas ${NC}\n"
 echo "========================================"
